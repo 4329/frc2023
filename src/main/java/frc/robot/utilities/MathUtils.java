db58d144fbaae78c8d1ab12a4ce129016f919683
MBorchert <matthew.borchert@lhssc.org>
2023-02-01 19:42:04 -0600

    made initial method

--- src/main/java/frc/robot/utilities/MathUtils.java
+++ src/main/java/frc/robot/utilities/MathUtils.java
@@ -1,5 +1,11 @@
 package frc.robot.utilities;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMax.SoftLimitDirection;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import frc.robot.Constants;
 import frc.robot.Constants.*;
 
 /**
@@ -69,9 +75,30 @@ public class MathUtils {
    * @param meters
    * @return inches
    */
-  public static double MetersToInches(double meters) {
+  public static double metersToInches(double meters) {
 
     return meters * 39.37;
+  }
+
+  /**
+   * The default method to be used when creating a new CANSparkMax, which gives it
+   * basic settings for ease of use.
+   * 
+   * @param id
+   * @return a new CAN object
+   */
+  public static CANSparkMax createCANSparkMax(int id) {
+
+    CANSparkMax canToMake = new CANSparkMax(id, MotorType.kBrushless);
+    canToMake.restoreFactoryDefaults();
+    canToMake.clearFaults();
+    canToMake.enableSoftLimit(SoftLimitDirection.kForward, false);
+    canToMake.enableSoftLimit(SoftLimitDirection.kReverse, false);
+    canToMake.setSmartCurrentLimit(Constants.ModuleConstants.kDriveCurrentLimit);
+    canToMake.enableVoltageCompensation(Constants.DriveConstants.kVoltCompensation);
+    canToMake.setIdleMode(IdleMode.kCoast);
+    canToMake.burnFlash();
+    return canToMake;
   }
 
 }
