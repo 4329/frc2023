24b38b3e5de83f610996430e941f69b977077d9c
Cameron Bell <cameron.bell@lhssc.org>
2023-02-04 14:58:52 -0600

    Added WristSubsystem.java

---
+++ src/main/java/frc/robot/subsystems/WristSubsystem.java
@@ -0,0 +1,68 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.SoftLimitDirection;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.utilities.SparkFactory;
+
+public class WristSubsystem extends SubsystemBase {
+
+    private CANSparkMax wristMotor;
+    private RelativeEncoder wristEncoder;
+    private SparkMaxPIDController wristPID;
+    private double setpoint;
+    
+    public WristSubsystem() {
+
+        wristMotor = SparkFactory.createCANSparkMax(Constants.CANIDConstants.wristRotate);
+        wristPID = wristMotor.getPIDController();
+        wristEncoder = wristMotor.getEncoder();
+        wristMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+        wristMotor.enableSoftLimit(SoftLimitDirection.kForward, true);
+        wristMotor.enableSoftLimit(SoftLimitDirection.kReverse, true);
+        wristMotor.setSoftLimit(SoftLimitDirection.kForward, -1);
+        wristMotor.setSoftLimit(SoftLimitDirection.kReverse, 1);
+        wristEncoder.setPosition(0);
+        wristPID.setP(0.1);
+        wristPID.setI(1e-4);
+        wristPID.setD(1);
+        wristPID.setIZone(0);
+        wristPID.setFF(0);
+        wristPID.setOutputRange(-1, 1);
+        wristMotor.burnFlash();
+        setpoint = 0;
+    }
+
+    public void setWristPosition(Double setpoint) {
+        
+        this.setpoint = setpoint;
+
+        wristPID.setReference(setpoint, CANSparkMax.ControlType.kPosition);
+    }
+    public void wristUp() {
+
+        setpoint -= Constants.WristConstants.wristRotationSpeed; 
+        wristPID.setReference(setpoint, CANSparkMax.ControlType.kPosition);
+    }
+
+    public void wristDown() {
+
+        setpoint += Constants.WristConstants.wristRotationSpeed; 
+        wristPID.setReference(setpoint, CANSparkMax.ControlType.kPosition);
+    }
+
+    public void wristStop() {
+
+        wristMotor.set(0);
+    }
+
+    public boolean wristAtSetpoint() {
+
+        return false;
+    }
+    
+}
